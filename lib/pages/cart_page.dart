844ea838d038cccbca40e7514cde8cfd9726b482
D-Mosfet <dhery081200@gmail.com>
2023-07-31 21:17:33 +0700

    update

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,142 +1,98 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:provider/provider.dart';
+import 'package:groceryapp/pages/home_page.dart';
 
-import '../model/cart_model.dart';
+import 'package:http/http.dart' as http;
 
-class CartPage extends StatelessWidget {
-  const CartPage({super.key});
+class CreateAlat extends StatefulWidget {
+  const CreateAlat({super.key});
+
+  @override
+  State<CreateAlat> createState() => _CreateAlatState();
+}
+
+class _CreateAlatState extends State<CreateAlat> {
+  final itemController = TextEditingController();
+  final priceController = TextEditingController();
+
+  Future<void> addData() async {
+    Map<String, String> headers = {
+      'Content-Type': 'application/json',
+    };
+    final response =
+        await http.post(Uri.parse('http://localhost:1337/api/freshmarts'),
+            headers: headers,
+            body: jsonEncode({
+              'data': {
+                'item': itemController.text,
+                'price': priceController.text,
+              }
+            }));
+
+    if (response.statusCode == 200) {
+      itemController.clear();
+      priceController.clear();
+    } else {
+      throw Exception('Failed to add data');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        iconTheme: IconThemeData(
-          color: Colors.grey[800],
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+        centerTitle: true,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Color.fromARGB(255, 216, 49, 49)),
+          onPressed: () {
+            Navigator.pop(
+              context,
+              MaterialPageRoute(builder: (context) => HomePage()),
+            );
+          },
+        ),
+        title: Text(
+          "Tambah Barang",
+          style: TextStyle(fontSize: 20, color: Colors.black),
         ),
       ),
-      body: Consumer<CartModel>(
-        builder: (context, value, child) {
-          return Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              // Let's order fresh items for you
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 24.0),
-                child: Text(
-                  "My Cart",
-                  style: GoogleFonts.notoSerif(
-                    fontSize: 36,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-
-              // list view of cart
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.all(12.0),
-                  child: ListView.builder(
-                    itemCount: value.cartItems.length,
-                    padding: EdgeInsets.all(12),
-                    itemBuilder: (context, index) {
-                      return Padding(
-                        padding: const EdgeInsets.all(12.0),
-                        child: Container(
-                          decoration: BoxDecoration(
-                              color: Colors.grey[200],
-                              borderRadius: BorderRadius.circular(8)),
-                          child: ListTile(
-                            leading: Image.asset(
-                              value.cartItems[index][2],
-                              height: 36,
-                            ),
-                            title: Text(
-                              value.cartItems[index][0],
-                              style: const TextStyle(fontSize: 18),
-                            ),
-                            subtitle: Text(
-                              '\$' + value.cartItems[index][1],
-                              style: const TextStyle(fontSize: 12),
-                            ),
-                            trailing: IconButton(
-                              icon: const Icon(Icons.cancel),
-                              onPressed: () =>
-                                  Provider.of<CartModel>(context, listen: false)
-                                      .removeItemFromCart(index),
-                            ),
-                          ),
-                        ),
-                      );
-                    },
-                  ),
-                ),
-              ),
-
-              // total amount + pay now
-
-              Padding(
-                padding: const EdgeInsets.all(36.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8),
-                    color: Colors.green,
-                  ),
-                  padding: const EdgeInsets.all(24),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            'Total Price',
-                            style: TextStyle(color: Colors.green[200]),
-                          ),
-
-                          const SizedBox(height: 8),
-                          // total price
-                          Text(
-                            '\$${value.calculateTotal()}',
-                            style: const TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white,
-                            ),
-                          ),
-                        ],
-                      ),
-
-                      // pay now
-                      Container(
-                        decoration: BoxDecoration(
-                          border: Border.all(color: Colors.green.shade200),
-                          borderRadius: BorderRadius.circular(28),
-                        ),
-                        padding: const EdgeInsets.all(12),
-                        child: Row(
-                          children: const [
-                            Text(
-                              'Pay Now',
-                              style: TextStyle(color: Colors.white),
-                            ),
-                            Icon(
-                              Icons.arrow_forward_ios,
-                              size: 16,
-                              color: Colors.white,
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              )
-            ],
-          );
-        },
+      body: Container(
+        padding: EdgeInsets.all(20),
+        child: Column(children: [
+          TextField(
+            controller: itemController,
+            decoration: InputDecoration(
+              labelText: 'Nama',
+            ),
+          ),
+          TextField(
+            controller: priceController,
+            decoration: InputDecoration(
+              labelText: 'Harga',
+            ),
+          ),
+          SizedBox(height: 10),
+          ElevatedButton(
+            onPressed: () {
+              addData();
+              Navigator.of(context).pushReplacement(MaterialPageRoute(
+                builder: (context) => const HomePage(),
+              ));
+            },
+            child: Text(
+              'Create',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+            ),
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.red,
+            ),
+          ),
+        ]),
       ),
     );
   }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,38 +1,127 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:groceryapp/pages/cart_page.dart';
 import 'package:provider/provider.dart';
 import '../components/grocery_item_tile.dart';
 import '../model/cart_model.dart';
-import 'cart_page.dart';
+
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  var datajson;
+  int totalData = 0;
+  bool isLoading = true;
+  final itemController = TextEditingController();
+  final priceController = TextEditingController();
+  @override
+  void initState() {
+    super.initState();
+    _fetchData();
+  }
+
+  Future<void> _fetchData() async {
+    try {
+      final response = await http.get(
+        Uri.parse('http://localhost:1337/api/freshmarts'),
+      );
+
+      if (response.statusCode == 200) {
+        setState(() {
+          datajson = jsonDecode(response.body);
+          totalData = datajson["meta"]["pagination"]["total"];
+          isLoading = false;
+        });
+      } else {
+        throw Exception('Failed to fetch data');
+      }
+    } catch (error) {
+      setState(() {
+        isLoading = false;
+      });
+      throw Exception('Failed to connect to the server');
+    }
+  }
+
+  Future<void> updateData(String id) async {
+    final response = await http.put(
+      Uri.parse('http://localhost:1337/api/freshmarts/$id'),
+      headers: {'Content-Type': 'application/json'},
+      body: json.encode({
+        'data': {
+          'item': itemController.text,
+          'price': priceController.text,
+        }
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      _fetchData();
+      itemController.clear();
+      priceController.clear();
+    } else {
+      throw Exception('Failed to update data');
+    }
+  }
+
+  Future<void> _confirmDelete(String id) async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Delete Item'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: const <Widget>[
+                Text('Are you sure you want to delete this item?'),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Cancel'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: const Text('Delete'),
+              onPressed: () async {
+                Navigator.of(context).pop();
+                await deleteData(id);
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Future<void> deleteData(String id) async {
+    final response = await http
+        .delete(Uri.parse('http://localhost:1337/api/freshmarts/$id'));
+
+    if (response.statusCode == 200) {
+      _fetchData();
+    } else {
+      throw Exception('Failed to delete data');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         elevation: 0,
-        leading: Padding(
-          padding: const EdgeInsets.only(left: 24.0),
-          child: Icon(
-            Icons.location_on,
-            color: Colors.grey[700],
-          ),
-        ),
-        title: Text(
-          'Sydney, Australia',
-          style: TextStyle(
-            fontSize: 16,
-            color: Colors.grey[700],
-          ),
-        ),
         centerTitle: false,
         actions: [
           Padding(
@@ -53,30 +142,22 @@ class _HomePageState extends State<HomePage> {
       ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.black,
-        onPressed: () => Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) {
-              return CartPage();
-            },
-          ),
-        ),
-        child: const Icon(Icons.shopping_bag),
+        onPressed: () {
+          Navigator.of(context).push(MaterialPageRoute(
+            builder: (context) => CreateAlat(),
+          ));
+        },
+        child: const Icon(Icons.add_box_rounded),
       ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           const SizedBox(height: 48),
-
-          // good morning bro
           const Padding(
             padding: EdgeInsets.symmetric(horizontal: 24.0),
             child: Text('Good morning,'),
           ),
-
           const SizedBox(height: 4),
-
-          // Let's order fresh items for you
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 24.0),
             child: Text(
@@ -87,53 +168,100 @@ class _HomePageState extends State<HomePage> {
               ),
             ),
           ),
-
           const SizedBox(height: 24),
-
           const Padding(
             padding: EdgeInsets.symmetric(horizontal: 24.0),
             child: Divider(),
           ),
-
           const SizedBox(height: 24),
-
-          // categories -> horizontal listview
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 24.0),
             child: Text(
               "Fresh Items",
               style: GoogleFonts.notoSerif(
-                //fontWeight: FontWeight.bold,
                 fontSize: 18,
               ),
             ),
           ),
-
-          // recent orders -> show last 3
           Expanded(
-            child: Consumer<CartModel>(
-              builder: (context, value, child) {
-                return GridView.builder(
-                  padding: const EdgeInsets.all(12),
-                  itemCount: value.shopItems.length,
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    childAspectRatio: 1 / 1.2,
-                  ),
-                  itemBuilder: (context, index) {
-                    return GroceryItemTile(
-                      itemName: value.shopItems[index][0],
-                      itemPrice: value.shopItems[index][1],
-                      imagePath: value.shopItems[index][2],
-                      color: value.shopItems[index][3],
-                      onPressed: () =>
-                          Provider.of<CartModel>(context, listen: false)
-                              .addItemToCart(index),
-                    );
-                  },
-                );
-              },
-            ),
+            child: isLoading
+                ? Center(child: CircularProgressIndicator())
+                : totalData == 0
+                    ? Center(child: Text('No data available.'))
+                    : Consumer<CartModel>(
+                        builder: (context, value, child) {
+                          return GridView.builder(
+                            padding: const EdgeInsets.all(12),
+                            itemCount: totalData,
+                            gridDelegate:
+                                const SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: 2,
+                              childAspectRatio: 1 / 1.2,
+                            ),
+                            itemBuilder: (data, index) {
+                              final itemId =
+                                  datajson['data'][index]['id'].toString();
+                              return GestureDetector(
+                                onTap: () {
+                                  showDialog(
+                                    context: context,
+                                    builder: (BuildContext context) {
+                                      return AlertDialog(
+                                        title: Text('Update Data'),
+                                        content: Column(
+                                          mainAxisSize: MainAxisSize.min,
+                                          children: [
+                                            TextField(
+                                              controller: itemController,
+                                              decoration: InputDecoration(
+                                                labelText: 'Nama',
+                                              ),
+                                            ),
+                                            TextField(
+                                              controller: priceController,
+                                              decoration: InputDecoration(
+                                                labelText: 'Harga',
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                        actions: [
+                                          TextButton(
+                                            child: Icon(Icons.cancel),
+                                            onPressed: () {
+                                              Navigator.of(context).pop();
+                                            },
+                                          ),
+                                          TextButton(
+                                            child: Icon(Icons.change_circle),
+                                            onPressed: () {
+                                              updateData(datajson['data'][index]
+                                                      ['id']
+                                                  .toString());
+                                              Navigator.of(context).pop();
+                                            },
+                                          ),
+                                        ],
+                                      );
+                                    },
+                                  );
+                                },
+                                child: GroceryItemTile(
+                                  itemName: datajson['data'][index]
+                                      ['attributes']['item'],
+                                  itemPrice:
+                                      '   ${datajson['data'][index]['attributes']['price']}',
+                                  imagePath: value.shopItems[index][2],
+                                  color: value.shopItems[index][3],
+                                  onPressed: () {
+                                    _confirmDelete(itemId);
+                                  },
+                                ),
+                              );
+                            },
+                          );
+                        },
+                      ),
           ),
         ],
       ),
